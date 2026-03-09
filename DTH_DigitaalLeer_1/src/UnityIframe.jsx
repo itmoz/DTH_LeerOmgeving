69d18043ef5e6ab8c99a9a9eb1096b5b4eb8d176
Leon van den Heuvel <leonheuveltje@gmail.com>
2026-03-09 10:03:25 +0100

    added unity game for testing

---
+++ DTH_DigitaalLeer_1/src/UnityIframe.jsx
@@ -0,0 +1,9 @@
+export default function UnityIframe() {
+  return (
+    <iframe
+      src="/unity/index.html"
+      style={{ width: '960px', height: '641.5px', border: 0 }}
+      title="Unity WebGL game"
+    />
+  );
+}
