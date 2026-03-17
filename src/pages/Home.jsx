e7faae7ed18a8b366f1c6e5b88e495e8ab8d3264
Masis21/Garen <antranik-garen@hotmail.com>
2026-03-17 11:12:22 +0100

    progress commit: working on sidebar and AppRoutes for the lessons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,19 @@
 import '../Styling/Home.css'
 import Button from '../WebsiteElements/Buttons/Button';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Home() {
-
-  function handleClick() {
-    alert("Button clicked!");
-  }
+  const navigate = useNavigate();
 
   return (
     <div className="home-container">
       <h1>DigiLeer!</h1>
       <p>This is the main page of the application.</p>
 
-      <Button onClick={handleClick}>
+      <Button onClick={() => navigate('/login')}>
         Start Learning
       </Button>
     </div>
-  )
+  );
 }
