78e9d72528864bd28381b5132c01f976c19e488b
Masis21/Garen <antranik-garen@hotmail.com>
2026-03-31 12:34:36 +0200

    added video player support

--- src/pages/GMHelden1Lessons/GmHelden1Lesson3.jsx
+++ src/pages/GMHelden1Lessons/GmHelden1Lesson3.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import VideoPlayer from "../../WebsiteElements/ImagesAndVideos/VideoPlayer.jsx";
 
+import TestVideo from "../../Video/Schatkist_Boek_en_Verblindend_Licht.mp4";
 
 export default function GmHelden1Lesson3() {
   return (
-    <div>
-      <h1>GmHelden1Lesson3</h1>
-      <p>This is the content of GmHelden1Lesson3.</p>
-    </div>
+    <VideoPlayer
+      src={TestVideo}
+      poster="https://www.w3schools.com/html/img_the_sunset.jpg"
+      title="GmHelden1Lesson3"
+      width="50%"
+    />
+
   );
 }
