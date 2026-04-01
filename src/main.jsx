9615c419501205403b9f00ba9d4d0c70811872db
Leon van den Heuvel <leonheuveltje@gmail.com>
2026-04-01 13:42:52 +0200

    added custom bootstrap ability and code cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './index.css'
 import App from './App.jsx'
+
+import './scss/styles.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
